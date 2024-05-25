4ae5403084c89c9b3e283256e8e07755eae03cc3
Hoai2801 <dreamhoaihack@gmail.com>
2024-05-25 01:16:43 -0700

    update: VD2 of BT4 in lab02

---
+++ Lab02/BT4/VD2/VD2/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace VD2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            comboBox1 = new ComboBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F);
+            label1.Location = new Point(204, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(385, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Chào mừng bạn đến với plpsoft.vn";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(280, 100);
+            label2.Name = "label2";
+            label2.Size = new Size(63, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Chọn màu";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Yellow", "Red", "Blue", "Black" });
+            comboBox1.Location = new Point(357, 98);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(287, 159);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Đổi màu";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(390, 159);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 4;
+            button2.Text = "Thoát";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(comboBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private ComboBox comboBox1;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ Lab02/BT4/VD2/VD2/Form1.cs
@@ -0,0 +1,43 @@
+﻿namespace VD2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult dg = MessageBox.Show("Bạn muốn đóng chương trình", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dg == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Yellow")
+            {
+                label1.BackColor = Color.Red;
+                label1.ForeColor = Color.Yellow;
+            }
+            if (comboBox1.Text == "Red")
+            {
+                label1.BackColor = Color.Yellow;
+                label1.ForeColor = Color.Red;
+            }
+            if (comboBox1.Text == "Blue")
+            {
+                label1.BackColor = Color.Yellow;
+                label1.ForeColor = Color.Blue;
+            }
+            if (comboBox1.Text == "Black")
+            {
+                label1.BackColor = Color.White;
+                label1.ForeColor = Color.Black;
+            }
+        }
+    }
+}
