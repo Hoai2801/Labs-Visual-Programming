eff4a2860e0cf3832611db3b71c0c0738de2372e
Hoai2801 <dreamhoaihack@gmail.com>
2024-05-24 18:18:41 -0700

    update: BT3 of Lab02

---
+++ Lab02/BT3/BT3/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace BT3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            checkBox5 = new CheckBox();
+            checkBox4 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox1 = new CheckBox();
+            groupBox2 = new GroupBox();
+            groupBox3 = new GroupBox();
+            radioButton5 = new RadioButton();
+            radioButton4 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            groupBox4 = new GroupBox();
+            button1 = new Button();
+            button2 = new Button();
+            groupBox1.SuspendLayout();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(checkBox5);
+            groupBox1.Controls.Add(checkBox4);
+            groupBox1.Controls.Add(checkBox3);
+            groupBox1.Controls.Add(checkBox2);
+            groupBox1.Controls.Add(checkBox1);
+            groupBox1.Controls.Add(groupBox2);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(151, 202);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Sở thích của bạn";
+            // 
+            // checkBox5
+            // 
+            checkBox5.AutoSize = true;
+            checkBox5.Location = new Point(14, 166);
+            checkBox5.Name = "checkBox5";
+            checkBox5.Size = new Size(76, 19);
+            checkBox5.TabIndex = 6;
+            checkBox5.Text = "Đi du lịch";
+            checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(14, 125);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(89, 19);
+            checkBox4.TabIndex = 5;
+            checkBox4.Text = "Đi mua sắm";
+            checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(14, 89);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(98, 19);
+            checkBox3.TabIndex = 4;
+            checkBox3.Text = "Chơi thể thao";
+            checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(14, 56);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(81, 19);
+            checkBox2.TabIndex = 3;
+            checkBox2.Text = "Xem phim";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(14, 21);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(84, 19);
+            checkBox1.TabIndex = 2;
+            checkBox1.Text = "Nghe nhạc";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Location = new Point(373, -95);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(69, 203);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "groupBox2";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(radioButton5);
+            groupBox3.Controls.Add(radioButton4);
+            groupBox3.Controls.Add(radioButton3);
+            groupBox3.Controls.Add(radioButton2);
+            groupBox3.Controls.Add(radioButton1);
+            groupBox3.Controls.Add(groupBox4);
+            groupBox3.Location = new Point(192, 12);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(151, 202);
+            groupBox3.TabIndex = 1;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Màu bạn thích";
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Location = new Point(15, 166);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(45, 19);
+            radioButton5.TabIndex = 6;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "Tím";
+            radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(15, 124);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(55, 19);
+            radioButton4.TabIndex = 5;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "Hồng";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(15, 89);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(51, 19);
+            radioButton3.TabIndex = 4;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Vàng";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(15, 56);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(54, 19);
+            radioButton2.TabIndex = 3;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Trắng";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(15, 24);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(40, 19);
+            radioButton1.TabIndex = 2;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Đỏ";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            groupBox4.Location = new Point(373, -95);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(69, 203);
+            groupBox4.TabIndex = 1;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "groupBox4";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(26, 220);
+            button1.Name = "button1";
+            button1.Size = new Size(119, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Sở thích của bạn";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(226, 220);
+            button2.Name = "button2";
+            button2.Size = new Size(98, 23);
+            button2.TabIndex = 3;
+            button2.Text = "Màu bạn thích";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(377, 266);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private CheckBox checkBox5;
+        private CheckBox checkBox4;
+        private CheckBox checkBox3;
+        private CheckBox checkBox2;
+        private CheckBox checkBox1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
+        private GroupBox groupBox4;
+        private RadioButton radioButton5;
+        private RadioButton radioButton4;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ Lab02/BT3/BT3/Form1.cs
@@ -0,0 +1,53 @@
+﻿namespace BT3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            if (checkBox1.Checked)
+            {
+                result += checkBox1.Text + "\n";
+            }
+            if (checkBox2.Checked)
+            {
+                result += checkBox2.Text + "\n";
+            }
+            if (checkBox3.Checked)
+            {
+                result += checkBox3.Text + "\n";
+            }
+            if (checkBox4.Checked)
+            {
+                result += checkBox4.Text + "\n";
+            }
+            if (checkBox5.Checked)
+            {
+                result += checkBox5.Text + "\n";
+            }
+            if (result.Length > 0)
+            {
+                MessageBox.Show("Sở thích của bạn là: \n" + result);
+            }
+            else MessageBox.Show("Không có sở thích nào mà bạn chọn!!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            if (radioButton1.Checked) result = radioButton1.Text + "\n";
+            if (radioButton2.Checked) result = radioButton2.Text + "\n";
+            if (radioButton3.Checked) result = radioButton3.Text + "\n";
+            if (radioButton4.Checked) result = radioButton4.Text + "\n";
+            if (radioButton5.Checked) result = radioButton5.Text + "\n";
+            if (result.Length > 0) MessageBox.Show("Màu bạn thích là: \n" + result);
+            else MessageBox.Show("Không có màu nào bạn chọn!!");
+
+        }
+    }
+}
